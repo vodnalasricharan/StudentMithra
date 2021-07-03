3fb02a7f4dc368f309b4e6b17f1303d45276cf81
Sricharan <vodnalasricharan@gmail.com>
2021-07-03 23:33:38 +0530

    re configured folder structure

--- studentmithra/settings.py
+++ studentmithra/settings.py
@@ -141,7 +141,7 @@ if DEBUG:
         #'/var/www/static/',
     ]
 else:
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 #     #'/var/www/static/',
@@ -149,7 +149,7 @@ else:
 # STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
