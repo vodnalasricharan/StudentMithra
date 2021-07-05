6e64813c1d2a21174ec9b3a985fe84ea77462404
Sricharan <vodnalasricharan@gmail.com>
2021-07-05 08:51:07 +0530

    updated new settings

--- studentmithra/settings.py
+++ studentmithra/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -174,3 +175,6 @@ EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
