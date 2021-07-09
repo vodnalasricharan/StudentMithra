5d33d58f5e93e24d76b27306b470415710b4f6a3
Sricharan <vodnalasricharan@gmail.com>
2021-07-09 22:30:07 +0530

    redeploy

--- studentmithra/settings.py
+++ studentmithra/settings.py
@@ -168,8 +168,8 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'kanyarasi.ene@gmail.com'
-# EMAIL_HOST_PASSWORD = 'kanyarasi@mail1'
+EMAIL_HOST_USER = 'kanyarasi.ene@gmail.com'
+EMAIL_HOST_PASSWORD = 'kanyarasi@mail1'
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD=os.environ['EMAIL_HOST_PASSWORD']
 
