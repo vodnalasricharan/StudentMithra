d2d189984355405f09522d1be11d4c1a2cd60b57
Sricharan <vodnalasricharan@gmail.com>
2021-07-29 11:40:49 +0530

    Update settings.py

--- studentmithra/settings.py
+++ studentmithra/settings.py
@@ -160,5 +160,5 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kanyarasi.ene@gmail.com'
-EMAIL_HOST_PASSWORD = 'kanyarasi@mail1'
+# EMAIL_HOST_USER = 'mailid'
+# EMAIL_HOST_PASSWORD = 'password'
